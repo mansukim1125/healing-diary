4ba618aa171a14a67c7ffbaf396ffa75faaeffb4
joonseok kim <mansukim1125@gmail.com>
2025-01-04 01:35:39 +0900

    feat(refactor): header component 분리

---
+++ app/components/header/header.component.tsx
@@ -0,0 +1,9 @@
+import { CardHeader, CardTitle } from '@/components/ui/card';
+
+export default function HeaderComponent() {
+	return (
+		<CardHeader>
+			<CardTitle className="text-center text-2xl font-bold">마음 치유 공간</CardTitle>
+		</CardHeader>
+	);
+}
